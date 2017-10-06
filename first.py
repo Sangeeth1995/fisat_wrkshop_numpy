b2d6cdda84c9df692e00747752ba08442575ad52
Sangeeth1995 <discoversangeeth@gmail.com>
2017-10-06 05:48:02 -0400

    Update first.py

--- first.py
+++ first.py
@@ -3,5 +3,21 @@ from sklearn import datasets, linear_model
 import matplotlib.pyplot as pit
 def generate_data():
 	np.random.seed(0)
-	x,y =  datasets_make_moons(200, noise = 0.20)
-	return x,y
+	X,y =  datasets_make_moons(200, noise = 0.20)
+	return X,y
+def visualize(X,y,clf):
+	plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+	plt.show()
+
+def classify(X, y):
+	clf = linear_model.LogisticRegressionCV()
+	clf.fit(X,y)
+	return clf
+def main():
+	X,y =  generate_data()
+	clf = classify(X, y)
+	visualize(X, y , clf)
+
+if __name__ == "__main__":
+	main()
+
